0b1fe7bcdf20f0decb22e3d81e0b61cb51423b7e
aarohitiwari123 <64591603+aarohitiwari123@users.noreply.github.com>
2020-05-08 18:19:24 +0530

    Add files via upload

---
+++ pr 20.js
@@ -0,0 +1,31 @@
+var car, checking;
+var weight,speed;
+
+function setup() {
+  createCanvas(1600,400);
+  speed = random(55,90);
+  weight = random(400,1500);
+  car = createSprite(50, 200, 50, 50);
+  checking = createSprite(1500,200,60,height/2);
+  car.velocityX = speed;
+}
+
+function draw() {
+  background(155,25,255);  
+
+  if(checking.x - car.x <(car.width + checking.width)/2) 
+  {car.velocityX =0;
+   var deformation=0.5 *weight * speed* speed/22509;
+   if(deformation > 180) {
+   car.shapecolor =color(255,0,0); 
+  }
+  if(deformation <180 && deformation>100) 
+  {car. shapeColor =color(230, 230,0);
+   } 
+  if(deformation< 100){
+  car. shapeColor =color(0,255,0); 
+  } 
+}
+  
+  drawSprites();
+}
